9e79cda9e1712fe953ef517e9ede6517e5bacdf3
gabrieldinu144 <marian_gabriel.dinu@stud.acs.upb.ro>
2024-11-13 15:01:35 +0200

    Added A and B1 exercises

--- PracticalTest01Var06/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var06/PracticalTest01Var06MainActivity.kt
+++ PracticalTest01Var06/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var06/PracticalTest01Var06MainActivity.kt
@@ -1,6 +1,9 @@
 package ro.pub.cs.systems.eim.practicaltest01var06
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,27 @@ class PracticalTest01Var06MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val button: Button = findViewById(R.id.play)
+        val val1: EditText = findViewById(R.id.val1)
+        val val2: EditText = findViewById(R.id.val2)
+        val val3: EditText = findViewById(R.id.val3)
+
+        val check1: CheckBox = findViewById(R.id.checkBox1)
+        val check2: CheckBox = findViewById(R.id.checkBox2)
+        val check3: CheckBox = findViewById(R.id.checkBox3)
+
+        val texts = listOf("1", "2", "3", "*")
+
+        button.setOnClickListener {
+            if (!check1.isChecked)
+                val1.setText(texts.random())
+            if (!check2.isChecked)
+                val2.setText(texts.random())
+            if (!check3.isChecked)
+                val3.setText(texts.random())
+        }
     }
+
+
 }
